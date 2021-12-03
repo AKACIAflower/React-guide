d6496fcf3f04d38549998f9b61823b237d3614eb
jinsoo <lgs2722@gmail.com>
2021-12-03 23:26:29 +0900

    Dealing with lists and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import React, { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Hamburger",
+    amount: 55.5,
+    date: new Date(2020, 10, 12),
+  },
+  {
+    id: "e2",
+    title: "Pizza",
+    amount: 70,
+    date: new Date(2021, 0, 10),
+  },
+  {
+    id: "e3",
+    title: "Chicken",
+    amount: 30.4,
+    date: new Date(2019, 5, 23),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      title: "Hamburger",
-      amount: 55.5,
-      date: new Date(2020, 10, 12),
-    },
-    {
-      title: "Pizza",
-      amount: 70,
-      date: new Date(2021, 0, 10),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (addExpense) => {
-    console.log(addExpense);
+    setExpenses((prevExpenses) => {
+      return [addExpense, ...prevExpenses];
+    });
   };
 
   return (
